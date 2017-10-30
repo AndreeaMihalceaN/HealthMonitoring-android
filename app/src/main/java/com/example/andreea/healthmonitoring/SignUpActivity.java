7c01e5039ca0310d3c5d9c7f6ef7d97cbc30eb9f
AndreeaMihalceaN <32798267+AndreeaMihalceaN@users.noreply.github.com>
2017-10-30 14:19:51 +0200

    Am facut interfata pentru Register

---
+++ app/src/main/java/com/example/andreea/healthmonitoring/SignUpActivity.java
@@ -0,0 +1,34 @@
+package com.example.andreea.healthmonitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText m_editTextUsername;
+    private EditText m_editTextFirstName;
+    private EditText m_editTextLastName;
+    private EditText m_editTextPassword;
+    private EditText m_editTextRetypePassword;
+    private EditText m_editTextHeight;
+    private EditText m_editTextWeight;
+    private Button m_buttonSubmit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        m_editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        m_editTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
+        m_editTextLastName = (EditText) findViewById(R.id.editTextLastName);
+        m_editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        m_editTextRetypePassword = (EditText) findViewById(R.id.editTextRetypePassword);
+        m_editTextHeight = (EditText) findViewById(R.id.editTextHeight);
+        m_editTextWeight = (EditText) findViewById(R.id.editTextWeight);
+        m_buttonSubmit = (Button) findViewById(R.id.buttonSignIn);
+    }
+}
